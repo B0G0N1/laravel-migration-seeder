7277b623c3c841246a97c9ceb0afbc4c989a2615
B0G0N1 <michelebogoni2001@gmail.com>
2024-10-01 02:55:12 +0200

    [FEAT] Setup project and create base layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Migration Seeder</title>
+
+    @vite('resources/js/app.js')
+    {{-- Collegamento ai file JS e CSS tramite Vite --}}
+</head>
+
+<body>
+    <div class="layout">
+        @include('partials.header')
+        {{-- Include il file header --}}
+
+        <main>
+            @yield('content')
+            {{-- Sezione per inserire il contenuto specifico delle pagine --}}
+        </main>
+
+        @include('partials.footer')
+        {{-- Include il file footer --}}
+    </div>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController as PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [PageController::class, 'homepage'])->name('homepage');
+// Definisce la rotta per la homepage, usa il metodo 'homepage' del PageController
